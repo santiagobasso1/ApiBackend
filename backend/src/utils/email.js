8fa980f8946392b8f59f445a69d705e02f520bd1
Santiago Basso <santiagobasso@hotmail.com>
2023-05-10 09:17:00 -0300

    Arreglado problema email

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -1,27 +1,36 @@
 import nodemailer from 'nodemailer'
-
+import 'dotenv/config.js'
 let transporter = nodemailer.createTransport({ //Genero la forma de enviar info desde mail (o sea, desde Gmail con x cuenta)
     host: 'smtp.gmail.com', //Defino que voy a utilizar un servicio de Gmail
     port: 465,
     secure: true,
     auth: {
-        user: "santiagobasso.sb@gmail.com", //Mail del que se envia informacion
-        pass: "jvemgqpsjdkdappw",
+        user: process.env.MAIL_SENDER, //Mail del que se envia informacion
+        pass: process.env.MAIL_PASSWORD,
         authMethod: 'LOGIN'
     }
 })
 
+
 export const sendEmail = async (req, res) => {
-    await transporter.sendMail({
-        from: 'Test Coder santiagobasso@hotmail.com',
-        to: req.session.user.email,
-        subject: "Sujeto del email",
-        html: `
-            <div>
-                <h2>PRUEBA ESTO ES UN H2</h2>
-            </div>
-        `,
-        attachments: []
-    })
-    res.send("Email enviado")
+    try{
+        await transporter.sendMail({
+            from: 'Test Coder santiagobasso@hotmail.com',
+            to: req.session.user.email,
+            subject: "Sujeto del email",
+            html: `
+                <div>
+                    <h2>Su Ticket: (No implementado aun)</h2>
+                </div>
+            `,
+            attachments: []
+        })
+        res.status(200).send({message:"Email enviado"})
+    }catch(error){
+        res.status(400).send({
+            message: "Problema con el email que envia",
+            error: error
+        })
+    }
+
 }
