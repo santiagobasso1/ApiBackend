4374972f4b36c27a6e0850f5b9e34c260004fb76
Santiago Basso <santiagobasso@hotmail.com>
2023-05-22 02:09:51 -0300

    Variables de entorno y dev&prod

--- backend/src/routes/mocking.routes.js
+++ backend/src/routes/mocking.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { fakeProductMock } from "../utils/mocking/productsMock.js";
+
 const routerMocking = Router()
 
-routerMocking.get("/mockingproducts", fakeProductMock)
+routerMocking.get("/mockingproducts", fakeProductMock) //No pido verificación de rol ya que es por esta entrega, luego le pediré roleVerification para admin
+
 
 
 export default routerMocking
--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -4,9 +4,9 @@ import { roleVerification } from "../utils/errorMessages.js";
 
 const routerProduct = Router();
 
-routerProduct.get('/',getProducts); //Este se saca en caso de querer ver los products por el front
+routerProduct.get('/',roleVerification(["Admin"]),getProducts); //Este se saca en caso de querer ver los products por el front
 routerProduct.get('/:pid',roleVerification(["Admin"]), getProduct);
-routerProduct.post('/', addProducts);
+routerProduct.post('/',roleVerification(["Admin"]), addProducts);
 routerProduct.put('/:pid',roleVerification(["Admin"]), updateProduct);
 routerProduct.delete('/:pid',roleVerification(["Admin"]), deleteProduct); //Solo al admin se le permite realizar esto.
 
