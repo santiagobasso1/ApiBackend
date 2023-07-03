dd0bd5acc686a6109a2bfcb25b3ccb7530d6408a
Santiago <santiagobasso@hotmail.com>
2023-07-03 06:08:40 -0300

    Se asocia al usuario cada document

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
 import multer from 'multer'
+import jwt from 'jsonwebtoken'
+import { findUserById } from '../services/UserService.js';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -15,6 +17,26 @@ const storage = multer.diskStorage({
 
 export const upload = multer({storage:storage})
 
-export const mensajeResultadoMulter=(req,res)=>{
+export const mensajeResultadoMulter=async(req,res)=>{
+    const cookie = req.cookies['userCookie']
+    if (!cookie) {
+        req.logger.fatal("Logued user not found")
+        return res.status(401).json({ error: "Logued user not found" })
+    }
+    const loguedUser = jwt.verify(cookie, process.env.JWT_SECRET).user;
+    const dbUser = await findUserById(loguedUser._id)
+    
+    req.files.forEach(document => {
+        if(dbUser.documents.some((dbDocument)=>{return dbDocument.name==document.filename})){
+            return ""
+        }
+        dbUser.documents.push({
+            name:document.filename,
+            reference:`${document.destination}/${document.filename}`
+        })
+    });
+    dbUser.lastConnection= Date.now()
+    await dbUser.save()
+    console.log(dbUser)
     res.status(200).json({message:"Upload image completed successfully"})
 }
